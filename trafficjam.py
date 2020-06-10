c2c341072c069cb711dd7997383a5a37e18a691c
dolic72 <dubravko@dolic.de>
2020-06-10 13:59:29 +0200

    Running version

--- trafficjam.py
+++ trafficjam.py
@@ -16,8 +16,8 @@ from selenium.webdriver.chrome.options import Options
 
 # Configuration
 # Needs to be outsourced in a config file
-outfile_path = '/home/ubuntu/images/'
-driver_path = '/home/ubuntu/driver/chromedriver'
+outfile_path = '/home/dolicd/Downloads/'
+driver_path = '/usr/bin/chromedriver'
 browser_path = '/usr/bin/google-chrome'
 url = 'https://www.google.de/maps/@47.8554527,12.1209407,14.25z/data=!5m1!1e1'
 naming_prefix = "gmro"
@@ -25,6 +25,9 @@ naming_prefix = "gmro"
 ## Timestamp has to be invoked in each new process
 initial_timestamp = datetime.datetime.fromtimestamp(
     time.time()).strftime('%Y%m%d%H%M%S')
+## Image file name (to be created)
+imgnm = outfile_path + naming_prefix + "-" + initial_timestamp + ".png"
+
 
 ## Colours to search for
 ## should be given fixed, no need to be changed 
@@ -37,23 +40,23 @@ col_space = [([0, 0, 192], [0, 0, 255]), ([32, 180, 0], [80, 202, 132]),
 
 
 ## Method get screenshot of trafficmap:
-def get_trafficmap(url, outfile=outfile_path + naming_prefix + "-" +
-                   initial_timestamp + ".png",
+def get_trafficmap(url,
+                   outfile=imgnm,
                    driver_path=driver_path,
                    browser_path=browser_path):
     if not url.startswith('http'):
         raise Exception('URLs need to start with "http"')
 
     chrome_options = Options()
-    chrome_options.add_argument("--no-sandbox")
+    #chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")  
     chrome_options.add_argument("--window-size=1280,800")
-    chrome_options.binary_location = driver_path
-    os.environ["webdriver.chrome.driver"] = browser_path
+    #chrome_options.binary_location = driver_path
+    #os.environ["webdriver.chrome.driver"] = browser_path
 
     driver = webdriver.Chrome(
         executable_path=driver_path,
-        chrome_options=chrome_options
+        options=chrome_options
     )
     driver.get(url)
     driver.implicitly_wait(3)
@@ -73,11 +76,12 @@ def create_tensor(imgfile, colors, colorspace, csv_count, csv_coord):
     spcnt = len(i.split("/")) - 1
     img = cv2.imread(i, 1)
     print("Processing ", i)
-    dt = i.split("/")[spcnt][5:13]   # Extract date
-    zt = i.split("/")[spcnt][13:17]  # Extract time
+    dt = initial_timestamp[0:8]   # Extract date
+    zt = initial_timestamp[8:]  # Extract time
     for f in colors:
         fr = d[f]
         try:
+            # create mask for color range per color 
             mask = cv2.inRange(img, np.array(fr[0]), np.array(fr[1]))
             u, c = np.unique(mask, return_counts=True)
             freq = np.asarray((u, c)).T
@@ -98,8 +102,9 @@ def create_tensor(imgfile, colors, colorspace, csv_count, csv_coord):
 
 
 ### Execution
-# Name of file
-fn = pfad + prefix + "-" + st + ".png"
-
-mapsc(url = url, outfile = fn)
-create_tensor(fn, colors = farben, colorspace = farbraum, csv_count = pfad + prefix + "-count.csv", csv_coord = pfad + prefix + "-coord.csv")
+get_trafficmap(url=url)
+create_tensor(imgnm,
+              colors=col_defs,
+              colorspace=col_space,
+              csv_count=outfile_path + naming_prefix + "-count.csv",
+              csv_coord=outfile_path + naming_prefix + "-coord.csv")
